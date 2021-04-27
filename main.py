3ad1317387840b9822a71d78f36761da56fdfbab
Artefok <Artefok2017@mail.ru>
2021-04-27 23:16:47 +0700

    New Update:     Add a button "Save" (Gui)     Front-end is ready! (Gui)     Add a sorting (Gui)

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ class Window(QWidget):
         self.cur = self.con.cursor()
         self.setWindowTitle("To-Do")
         self.setWindowIcon(QIcon('web.png'))
-        self.setGeometry(0, 0, 1520, 800)
+        self.setGeometry(100, 50, 1920, 1080)
 
-        self.layout = QHBoxLayout(self)
+        self.layout = QVBoxLayout(self)
+        self.prelayout = QHBoxLayout(self)
 
-        
-        self.grid = QGridLayout()
+        self.grid = QGridLayout(self)
 
         self.row = 0
         #===================
@@ -25,17 +25,23 @@ class Window(QWidget):
         
         self.label = QLabel('To-Do!', self)
         
-        self.btn = QPushButton('Настройки', self)
-        self.btn.resize(self.btn.sizeHint())
         self.plus = QPushButton("+", self)
-        self.plus.resize(600, 30)
+        self.plus.resize(700, 30)
         self.plus.clicked.connect(self.createTask)
         
+##        self.grid.addWidget(self.plus, 0, 2, 3, 2)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.plus)
+        self.all = QPushButton("Все")
+        self.active = QPushButton("Активные")
+        self.done = QPushButton("Сделанные")
 
-        self.grid.addWidget(self.label, 0, 1, 1, 2)
-        self.grid.addWidget(self.btn, 0, 4, 1, 1)
-        self.grid.addWidget(self.plus, 0, 2, 3, 2)
-
+        self.prelayout.addWidget(self.all)
+        self.prelayout.addWidget(self.active)
+        self.prelayout.addWidget(self.done)
+        
+        self.layout.addLayout(self.prelayout)
+        
         self.scrollArea = QScrollArea(self)
         self.scrollArea.setWidgetResizable(True)
         self.scrollAreaWidgetContents = QWidget()
@@ -43,10 +49,9 @@ class Window(QWidget):
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.layout.addWidget(self.scrollArea)
 
-        self.tasks.setSpacing(30)
-        self.tasks.setContentsMargins(30,70,30,70)
-
-        self.grid.addLayout(self.tasks, 2, 1, 4, 4)
+        self.tasks.setSpacing(10)
+        
+        self.layout.addLayout(self.tasks)
 
         #===================
         #CSS part
@@ -54,18 +59,68 @@ class Window(QWidget):
         self.label.setStyleSheet("""font-size: 14.5pt;
                                     font-family: MS Gothic 2;
                                     """)
-        self.btn.setStyleSheet("""font-size: 9pt;
-                                  font-family: MS Gothic 2;""")
+        self.all.setStyleSheet("""QPushButton{
+                                      margin-top: 40px;
+                                      margin-right: 0px;
+                                      margin-left: 0px;
+                                      border: 1px solid gray;
+                                  }
+                                  QPushButton:hover{
+                                      background-color: lightgray;
+                                  }
+                                """)
+        
+        self.active.setStyleSheet("""QPushButton{
+                                      margin-top: 40px;
+                                      margin-right: 0px;
+                                      margin-left: 0px;
+                                      border: 1px solid gray;
+                                  }
+                                  QPushButton:hover{
+                                      background-color: lightgray;
+                                  }
+                                  QPushButton:pressed{
+                                      box-shadow: 5px 10px 8px;
+                                  }
+                                """)
+        
+        self.done.setStyleSheet("""QPushButton{
+                                      margin-top: 40px;
+                                      margin-right: 0px;
+                                      margin-left: 0px;
+                                      border: 1px solid gray;
+                                  }
+                                  QPushButton:hover{
+                                      background-color: lightgray;
+                                  }
+                                """)
+        
         
         #====================
         self.btns1 = []
         self.btns2 = []
+        self.btns3 = []
         self.layouts = []
+        self.done_layouts = []
         self.lines = []
         self.dates = []
         self.texts = []
 
         self.show()
+
+    def save(self):
+        # твой код
+        pass
+
+    def edit(self):
+        # твой код
+        pass
+        
+    def delete(self):
+        # твой код
+        pass
+        
+        
     def createTask(self):
         global row
         self.layouts.append(QGridLayout())
@@ -74,12 +129,18 @@ class Window(QWidget):
         self.texts.append(QTextEdit(self))
         self.btns1.append(QPushButton("Изменить", self))
         self.btns2.append(QPushButton("Удалить", self))
+        self.btns3.append(QPushButton("Сохранить", self))
 
+        self.btns1[self.row].clicked.connect(self.edit)
+        self.btns2[self.row].clicked.connect(self.delete)
+        self.btns3[self.row].clicked.connect(self.save)
+        
         self.layouts[self.row].addWidget(self.lines[self.row], 0, 0)
         self.layouts[self.row].addWidget(self.dates[self.row], 0, 1)
         self.layouts[self.row].addWidget(self.texts[self.row], 0, 2)
         self.layouts[self.row].addWidget(self.btns1[self.row], 0, 3)
         self.layouts[self.row].addWidget(self.btns2[self.row], 0, 4)
+        self.layouts[self.row].addWidget(self.btns3[self.row], 0, 5)
 
         self.tasks.addLayout(self.layouts[self.row], self.row, 0)
         
@@ -91,6 +152,3 @@ if __name__ == '__main__':
     window = Window()
     App.setStyle('Fusion')
     sys.exit(App.exec())
-
-
-
