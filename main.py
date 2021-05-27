d4eb16d01a89a272b418ea77254b2d7ca304b84a
Artefok <83118027+Artefok@users.noreply.github.com>
2021-05-27 23:33:38 +0700

    FINAL VERSION - we ready to upload an .exe file.

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 from reminders import *
-import datetime
+from datetime import date
+from plyer import notification
 
 class Window(QWidget):
     def __init__(self):
@@ -12,7 +13,6 @@ class Window(QWidget):
         self.setWindowTitle("To-Do")
         self.setWindowIcon(QIcon('web.png'))
         self.setGeometry(100, 50, 1920, 1080)
-        self.cur_id = None
 
         self.layout = QVBoxLayout(self)
         self.prelayout = QHBoxLayout(self)
@@ -35,7 +35,10 @@ class Window(QWidget):
         self.layout.addWidget(self.plus)
         self.all = QPushButton("Все")
         self.active = QPushButton("Активные")
-        self.done = QPushButton("Сделанные")
+        self.done = QPushButton("Завершенные")
+        self.all.clicked.connect(self.all_tasks)
+        self.active.clicked.connect(self.active_tasks)
+        self.done.clicked.connect(self.done_tasks)
 
         self.prelayout.addWidget(self.all)
         self.prelayout.addWidget(self.active)
@@ -49,12 +52,12 @@ class Window(QWidget):
         self.tasks = QGridLayout(self.scrollAreaWidgetContents)
         self.scrollArea.setWidget(self.scrollAreaWidgetContents)
         self.layout.addWidget(self.scrollArea)
-        self.creating = False
 
         self.tasks.setSpacing(10)
         
         self.layout.addLayout(self.tasks)
 
+        self.createTaskWithStuff
 
         #===================
         #CSS part
@@ -105,11 +108,16 @@ class Window(QWidget):
         self.layouts1 = {}
         self.layouts2 = {}
         self.layouts3 = {}
+        self.layouts4 = {}
+        self.done_layouts = {}
+        self.str_louts = {}
         self.x_pos = 0
         self.y_pos = 0
 
         for task in self.db.read_all():
-            self.createTaskWithStuff(id=task[0], name=task[1], date_exec=task[2], text=task[3], date_until=task[4])
+            self.createTaskWithStuff(id=task[0], name=task[1], date_exec=task[2], text=task[3], date_until=task[4], status=task[5])
+
+        self.day_notifications()
 
         self.show()
 
@@ -119,11 +127,17 @@ class Window(QWidget):
         self.val_list = list(self.layouts2.values())
         self.position = self.val_list.index(self.widget)
         self.item = self.key_list[self.position]
-        for i in self.db.read_all():
-            if i[1] == "temp":
-                self.db.update(self.layouts[self.item][3].dateTime().toString(),self.layouts[self.item][4].toPlainText(), i[0], self.layouts[self.item][2].dateTime().toString(),self.layouts[self.item][0].task_id, self.layouts[self.item][1].text() if self.layouts[self.item][1].text() else None)
-                break
-        
+        self.cur_id = int(self.layouts[self.item][0].text())
+        self.date = self.layouts[self.item][2].date().toString('yyyy-MM-dd')
+        self.text = str(self.layouts[self.item][4].toPlainText())
+        self.date1 = self.layouts[self.item][3].date().toString('yyyy-MM-dd')
+        self.name = str(self.layouts[self.item][1].text())
+        print(self.date)
+        print(self.text)
+        print(self.date1)
+        print(self.name)
+
+        self.db.update(self.date, self.text, self.date1, self.cur_id, self.name  if self.name else None)
 
         self.layouts[self.item][1].setReadOnly(True)
         self.layouts[self.item][2].setReadOnly(True)
@@ -132,129 +146,62 @@ class Window(QWidget):
         self.layouts[self.item][5].setEnabled(True)
         self.layouts[self.item][6].setEnabled(False)
         self.layouts[self.item][7].setEnabled(True)
-        self.creating = False
 
     def edit(self):
-        if not self.creating:
-            self.widget = self.sender()
-            self.key_list = list(self.layouts3.keys())
-            self.val_list = list(self.layouts3.values())
-            self.position = self.val_list.index(self.widget)
-            self.item = self.key_list[self.position]
-            self.db.update(None, None, str(self.layouts[self.item][0].task_id), None, None, "temp")
-
-            self.layouts[self.item][1].setReadOnly(False)
-            self.layouts[self.item][2].setReadOnly(False)
-            self.layouts[self.item][3].setReadOnly(False)
-            self.layouts[self.item][4].setReadOnly(False)
-            self.layouts[self.item][6].setEnabled(True)
-            self.layouts[self.item][7].setEnabled(False)
+        self.widget = self.sender()
+        self.key_list = list(self.layouts3.keys())
+        self.val_list = list(self.layouts3.values())
+        self.position = self.val_list.index(self.widget)
+        self.item = self.key_list[self.position]
+        self.db.delete_the_task(self.layouts[self.item][0].text())
+        
+        self.layouts[self.item][1].setReadOnly(False)
+        self.layouts[self.item][2].setReadOnly(False)
+        self.layouts[self.item][3].setReadOnly(False)
+        self.layouts[self.item][4].setReadOnly(False)
+        self.layouts[self.item][6].setEnabled(True)
+        self.layouts[self.item][7].setEnabled(False)
         
     def delete(self):
-        if not self.creating:
-            self.widget = self.sender()
-            self.key_list = list(self.layouts1.keys())
-            self.val_list = list(self.layouts1.values())
-            self.position = self.val_list.index(self.widget)
-            self.item = self.key_list[self.position]
-            self.db.delete_the_task(self.layouts[self.item][0].task_id)
+        self.widget = self.sender()
+        self.key_list = list(self.layouts1.keys())
+        self.val_list = list(self.layouts1.values())
+        self.position = self.val_list.index(self.widget)
+        self.item = self.key_list[self.position]
+        self.db.delete_the_task(int(self.layouts[self.item][0].text()))
 
-            for i in self.layouts[self.item]:
-                i.deleteLater()
-                if self.layouts[self.task] == None:
-                    break
-                
-            self.y_pos -= 1
+        for i in self.layouts[self.item]:
+            i.deleteLater()
+            if self.layouts[self.item] == None:
+                break
+        del self.layouts[self.item]
+        
+        self.y_pos -= 1
 
         
         
     def createTask(self):
-        if not self.creating:
-            self.creating = True
-            self.y_pos = self.y_pos + 1
-            self.elem = []
-
-            self.db.create_a_task(None, None, None, self.y_pos, "temp")
-            
-            self.task = QGridLayout()
-            self.label = QLabel(str(self.y_pos))
-            for i in self.db.read_all():
-                if i[1] == "temp":
-                    self.label.task_id = i[0]
-                    break
-            self.line = QLineEdit()
-            self.date = QDateTimeEdit()
-            self.date.setDisplayFormat("ddd MMM d hh:mm:ss yyyy")
-            self.date1 = QDateTimeEdit()
-            self.date1.setDisplayFormat("ddd MMM d hh:mm:ss yyyy")
-            self.text = QTextEdit()
-            self.button = QPushButton(f"Delete")
-            self.button1 = QPushButton(f"Save")
-            self.button2 = QPushButton(f"Edit")
-
-
-            self.elem.append(self.label)
-            self.elem.append(self.line)
-            self.elem.append(self.date)
-            self.elem.append(self.date1)
-            self.elem.append(self.text)
-            self.elem.append(self.button)
-            self.elem.append(self.button1)
-            self.elem.append(self.button2)
-
-            self.task.addWidget(self.label, self.y_pos, 0)
-            self.task.addWidget(self.line, self.y_pos, 1)
-            self.task.addWidget(self.date, self.y_pos, 2)
-            self.task.addWidget(self.date1, self.y_pos, 3)
-            self.task.addWidget(self.text, self.y_pos, 4)
-            self.task.addWidget(self.button2, self.y_pos, 5)
-            self.task.addWidget(self.button1, self.y_pos, 6)
-            self.task.addWidget(self.button, self.y_pos, 7)
-
-            self.button.clicked.connect(self.delete)
-            self.button2.clicked.connect(self.edit)
-            self.button1.clicked.connect(self.save)
-
-            self.layouts[self.task] = self.elem
-            self.layouts1[self.task] = self.button
-            self.layouts2[self.task] = self.button1
-            self.layouts3[self.task] = self.button2
-            self.button2.setEnabled(False)
-            self.tasks.addLayout(self.task, self.y_pos, 0)
-
-    def createTaskWithStuff(self, id, name, date_exec, date_until, text):
         self.y_pos = self.y_pos + 1
         self.elem = []
 
         self.task = QGridLayout()
-        self.label = QLabel(str(self.y_pos))
-        self.label.task_id = id
-
+        self.label = QLabel(f"{self.y_pos}")
         self.line = QLineEdit()
-        self.line.setText(name)
-        self.line.setReadOnly(True)
 
         self.date = QDateTimeEdit()
-        self.date.setDateTime(QDateTime.fromString(date_exec, "dd.MM.yyyy hh:mm:ss"))
-        self.date.setDisplayFormat("dd.MM.yyyy hh:mm:ss")
-        self.date.setReadOnly(True)
+        self.date.setDisplayFormat("yyyy-MM-dd")
+        self.date.setDate(date.today())
 
         self.date1 = QDateTimeEdit()
-        self.date1.setDateTime(QDateTime.fromString(date_until, "dd.MM.yyyy hh:mm:ss"))
-        self.date1.setDisplayFormat("dd.MM.yyyy hh:mm:ss")
-        self.date1.setReadOnly(True)
-
+        self.date1.setDisplayFormat("yyyy-MM-dd")
+        self.date1.setDate(date.today())
 
         self.text = QTextEdit()
-        self.text.setText(text)
-        self.text.setReadOnly(True)
-
         self.button = QPushButton(f"Delete")
         self.button1 = QPushButton(f"Save")
         self.button2 = QPushButton(f"Edit")
+        self.button3 = QPushButton(f"Done")
 
-        
-        self.button1.setEnabled(False)
 
         self.elem.append(self.label)
         self.elem.append(self.line)
@@ -264,6 +211,7 @@ class Window(QWidget):
         self.elem.append(self.button)
         self.elem.append(self.button1)
         self.elem.append(self.button2)
+        self.elem.append(self.button3)
 
         self.task.addWidget(self.label, self.y_pos, 0)
         self.task.addWidget(self.line, self.y_pos, 1)
@@ -273,19 +221,142 @@ class Window(QWidget):
         self.task.addWidget(self.button2, self.y_pos, 5)
         self.task.addWidget(self.button1, self.y_pos, 6)
         self.task.addWidget(self.button, self.y_pos, 7)
+        self.task.addWidget(self.button3, self.y_pos, 8)
 
         self.button.clicked.connect(self.delete)
         self.button2.clicked.connect(self.edit)
         self.button1.clicked.connect(self.save)
+        self.button3.clicked.connect(self.doneTask)
         
         self.layouts[self.task] = self.elem
         self.layouts1[self.task] = self.button
         self.layouts2[self.task] = self.button1
         self.layouts3[self.task] = self.button2
+        self.layouts4[self.task] = self.button3
+        self.str_louts[str(self.task)] = self.task
 
+        self.button2.setEnabled(False)
         self.tasks.addLayout(self.task, self.y_pos, 0)
+
+        self.db.create_a_task(None, None, None, "Active", self.task, None, self.y_pos)
+
+    def doneTask(self):
+        self.widget = self.sender()
+        self.widget.setEnabled(False)
+        self.key_list = list(self.layouts4.keys())
+        self.val_list = list(self.layouts4.values())
+        self.position = self.val_list.index(self.widget)
+        self.item = self.key_list[self.position]
+        self.db.done(int(self.layouts[self.item][0].text()))
         
-          
+        
+    def createTaskWithStuff(self, id, name, date_exec, date_until, text, status):
+        self.y_pos += 1
+        self.elem = []
+
+        self.task = QGridLayout()
+        self.label = QLabel(f"{id}")
+
+        self.line = QLineEdit()
+        self.line.setText(name)
+        self.line.setReadOnly(True)
+
+        self.date = QDateEdit()
+        self.date.setDate(QDate.fromString(date_exec, "yyyy-MM-dd"))
+        self.date.setDisplayFormat("yyyy-MM-dd")
+        self.date.setReadOnly(True)
+
+        self.date1 = QDateEdit()
+        self.date1.setDate(QDate.fromString(date_until, "yyyy-MM-dd"))
+        self.date1.setDisplayFormat("yyyy-MM-dd")
+        self.date1.setReadOnly(True)
+
+        self.text = QTextEdit()
+        self.text.setText(text)
+        self.text.setReadOnly(True)
+
+        self.button = QPushButton(f"Delete")
+        self.button1 = QPushButton(f"Save")
+        self.button2 = QPushButton(f"Edit")
+        self.button3 = QPushButton(f"Done")
+
+        self.button1.setEnabled(False)
+
+        if status == "Done":
+            self.button3.setEnabled(False)
+
+        self.elem.append(self.label)
+        self.elem.append(self.line)
+        self.elem.append(self.date)
+        self.elem.append(self.date1)
+        self.elem.append(self.text)
+        self.elem.append(self.button)
+        self.elem.append(self.button1)
+        self.elem.append(self.button2)
+        self.elem.append(self.button3)
+
+        self.task.addWidget(self.label, id, 0)
+        self.task.addWidget(self.line, id, 1)
+        self.task.addWidget(self.date, id, 2)
+        self.task.addWidget(self.date1, id, 3)
+        self.task.addWidget(self.text, id, 4)
+        self.task.addWidget(self.button2, id, 5)
+        self.task.addWidget(self.button1, id, 6)
+        self.task.addWidget(self.button, id, 7)
+        self.task.addWidget(self.button3, id, 8)
+
+        self.button.clicked.connect(self.delete)
+        self.button2.clicked.connect(self.edit)
+        self.button1.clicked.connect(self.save)
+        self.button3.clicked.connect(self.doneTask)
+        
+        self.layouts[self.task] = self.elem
+        self.layouts1[self.task] = self.button
+        self.layouts2[self.task] = self.button1
+        self.layouts3[self.task] = self.button2
+        self.layouts4[self.task] = self.button3
+        self.str_louts[str(self.task)] = self.task
+
+        self.db.grid(int(self.layouts[self.task][0].text()), str(self.task))
+
+        self.tasks.addLayout(self.task, id, 0)
+    def all_tasks(self):
+        for task in self.db.read_all():
+            self.createTaskWithStuff(id=task[0], name=task[1], date_exec=task[2], text=task[3], date_until=task[4], status=task[5])
+
+    def active_tasks(self):
+        for task in self.db.read_all():
+            if task[5] == "Active":
+                self.createTaskWithStuff(id=task[0], name=task[1], date_exec=task[2], text=task[3], date_until=task[4], status=task[5])
+            else:
+                for i in self.layouts:
+                    for p in self.layouts[i]:
+                        p.deleteLater()
+                    del self.layouts[i][0]
+                    
+            
+    def done_tasks(self):
+
+        for task in self.db.read_all():
+            if task[5] == "Done":
+                self.createTaskWithStuff(id=task[0], name=task[1], date_exec=task[2], text=task[3], date_until=task[4], status=task[5])
+            else:
+                for i in self.layouts[self.str_louts[task[6]]]:
+                    i.deleteLater()
+                del self.layouts[self.str_louts[task[6]]]
+    def day_notifications(self):
+        self.str_of_tasks = "Сегодня должны быть сделаны задачи: "
+        for task in self.db.read_all():
+            if task[4] == str(date.today()):
+                if task[5] == "Active":
+                    self.str_of_tasks = self.str_of_tasks + str(task[1]) + ", "
+
+        self.str_of_tasks = self.str_of_tasks[:-2]
+        self.str_of_tasks = self.str_of_tasks + "."
+        notification.notify(
+            title="Напоминание", message=self.str_of_tasks, app_name="To-Do!", app_icon="web_notify.ico", timeout=10, ticker="", toast=False
+        )
+
 if __name__ == '__main__':
     App = QApplication(sys.argv)
     window = Window()
--- reminders/__init__.py
+++ reminders/__init__.py
@@ -13,54 +13,58 @@ class run:
         """
         self.con = sqlite3.connect(path)
         self.cur = self.con.cursor()
-        request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT)"
+        request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT, status TEXT)"
         self.cur.execute(request)
     def __enter__(self):
         self.con = sqlite3.connect("dbs/tasks.db")
         self.cur = self.con.cursor()
-        request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT)"
+        request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT, status TEXT)"
         self.cur.execute(request)
         return (self.con, self.cur)
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.con.close()
         if exc_val:
             raise Exception
-    def create_a_task(self, date_exec, text_exec, date_until, task_id, name=None):
+    def create_a_task(self, date_exec, text_exec, date_until, status, grid, name=None, task_id=None):
         """
         Creates a task.\n
         Создаёт напоминание.
         """
         if not name:
             name = "notnamed"
-        request = "INSERT INTO total_tasks(name, date, text, date_until, task_id) VALUES(?, ?, ?, ?, ?)"
-        self.cur.execute(request, (name, date_exec, text_exec, date_until, task_id))
+        if not task_id:
+            request = "INSERT INTO total_tasks(name, date, text, date_until, status, grid) VALUES(?, ?, ?, ?, ?, ?)"
+            self.cur.execute(request, (name, date_exec, text_exec, date_until, status, str(grid)))
+        else:
+             request = "INSERT INTO total_tasks(id, name, date, text, date_until, status, grid) VALUES(?, ?, ?, ?, ?, ?, ?)"
+             self.cur.execute(request, (task_id, name, date_exec, text_exec, date_until, status, str(grid)))
         self.commit()
     def delete_the_task(self, task_id):
         """
         Deletes the task. Requires task id\n
         Удаляет напоминание. Требуется идентификатор напоминания.
         """
-        request = "DELETE FROM total_tasks WHERE id = ?"
+        request = f"DELETE FROM total_tasks WHERE id = {task_id}"
+        self.cur.execute(request)
+        self.commit()
+    def read_one_id(self, task_id):
+        """
+        Reads the task. Requires task id\n
+        Считывает напоминание. Требуется идентификатор напоминания.
+        """
+        request = "SELECT * FROM total_tasks WHERE id = ?"
         self.cur.execute(request, (task_id,))
         self.commit()
-    # def read_one_id(self, task_id):
-    #     """
-    #     Reads the task. Requires task id\n
-    #     Считывает напоминание. Требуется идентификатор напоминания.
-    #     """
-    #     request = "SELECT * FROM total_tasks WHERE id = ?"
-    #     self.cur.execute(request, (task_id,))
-    #     self.commit()
-    #     return self.cur.fetchall()
-    # def read_one_day(self, date):
-    #     """
-    #     Reads the tasks on one day\n
-    #     Считывает напоминание на определённый день.
-    #     """
-    #     request = "SELECT * FROM total_tasks WHERE date = ?"
-    #     self.cur.execute(request, (date,))
-    #     self.commit()
-    #     return self.cur.fetchall()
+        return self.cur.fetchall()
+    def read_one_day(self, date):
+        """
+        Reads the tasks on one day\n
+        Считывает напоминание на определённый день.
+        """
+        request = "SELECT * FROM total_tasks WHERE date = ?"
+        self.cur.execute(request, (date,))
+        self.commit()
+        return self.cur.fetchall()
     def read_all(self):
         """
         Reads all tasks\n
@@ -70,81 +74,107 @@ class run:
         self.cur.execute(request)
         self.commit()
         return self.cur.fetchall()
-    # def clear(self):
-    #     """
-    #     Clears everything\n
-    #     Очищает базу данных.
-    #     """
-    #     request = "DROP TABLE IF EXISTS total_tasks"
-    #     self.cur.execute(request)
-    #     request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT)"
-    #     self.cur.execute(request)
+    def clear(self):
+        """
+        Clears everything\n
+        Очищает базу данных.
+        """
+        request = "DROP TABLE IF EXISTS total_tasks"
+        self.cur.execute(request)
+        request = "CREATE TABLE IF NOT EXISTS total_tasks(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, date TEXT, text TEXT, date_until TEXT)"
+        self.cur.execute(request)
     def con_exit(self):
         """
         Closes the connection\n
         Закрывает соединение.
         """
         self.con.close()
-    # def show_on_date(self, date):
-    #     """
-    #     Returns every reminder on date\n
-    #     Возвращает каждое напоминание на определённый день.
-    #     """
-    #     return self.read_one_day(date)
-    # def __get_today(self):
-    #     """
-    #     Returns today as date\n
-    #     Возвращает дату сегодняшнего дня.
-    #     """
-    #     date = f"{localtime(time()).tm_year}-{localtime(time()).tm_mon}-{localtime(time()).tm_mday}"
-    #     return date
-    # def show_today(self):
-    #     """
-    #     Returns every reminder today\n
-    #     Возвращает каждое напоминание на сегодня.
-    #     """
-    #     date = self.__get_today()
-    #     return self.read_one_day(date)
-    # def reminder(self):
-    #     """
-    #     Prints today's reminders\n
-    #     Печатает напоминания на сегодня.
-    #     """
-    #     for i in self.show_today():
-    #         print(f"id{i[0]}  {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
-    #         print(f"id{i[0]}  {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
-    # def reminder_on_date(self, date):
-    #     """
-    #     Prints date's reminders\n
-    #     Печатает напоминания на определённый день.
-    #     """
-    #     try:
-    #         for i in self.show_on_date(date):
-    #             print(f"id{i[0]} {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
-    #             print(f"id{i[0]} {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
-    #     except Exception as err:
-    #         print(err)
-    # def reminder_all(self):
-    #     """
-    #     Prints all reminders\n
-    #     Печатает все напоминания.
-    #     """
-    #     try:
-    #         for i in self.read_all():
-    #             print(f"id{i[0]} {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
-    #             print(f"id{i[0]} {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
-    #     except Exception as err:
-    #         print(err)
-    def update(self, date_until, text, task_id, date_exec, display_id, name=None):
+    def show_on_date(self, date):
+        """
+        Returns every reminder on date\n
+        Возвращает каждое напоминание на определённый день.
+        """
+        return self.read_one_day(date)
+    def __get_today(self):
+        """
+        Returns today as date\n
+        Возвращает дату сегодняшнего дня.
+        """
+        date = f"{localtime(time()).tm_year}-{localtime(time()).tm_mon}-{localtime(time()).tm_mday}"
+        return date
+    def show_today(self):
+        """
+        Returns every reminder today\n
+        Возвращает каждое напоминание на сегодня.
+        """
+        date = self.__get_today()
+        return self.read_one_day(date)
+    def reminder(self):
+        """
+        Prints today's reminders\n
+        Печатает напоминания на сегодня.
+        """
+        for i in self.show_today():
+            print(f"id{i[0]}  {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
+            print(f"id{i[0]}  {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
+    def reminder_on_date(self, date):
+        """
+        Prints date's reminders\n
+        Печатает напоминания на определённый день.
+        """
+        try:
+            for i in self.show_on_date(date):
+                print(f"id{i[0]} {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
+                print(f"id{i[0]} {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
+        except Exception as err:
+            print(err)
+    def reminder_all(self):
+        """
+        Prints all reminders\n
+        Печатает все напоминания.
+        """
+        try:
+            for i in self.read_all():
+                print(f"id{i[0]} {i[1]} from {i[2]} to {i[4]}.{i[5]}: {i[3]}")
+                print(f"id{i[0]} {i[1]} с {i[2]} до {i[4]}.{i[5]}: {i[3]}")
+        except Exception as err:
+            print(err)
+    def update(self, date1, text1, date_until1, task_id, name1=None):
         """
         Updates reminder\n
         Обновление напоминания
         """
-        if not name:
-            name = "notnamed"
-        request = "UPDATE total_tasks SET date_until=?, text=?, name=?, date=?, task_id=? WHERE id=?"
-        self.cur.execute(request, (date_until, text, name, date_exec, display_id, task_id))
+        if not name1:
+            name1 = "notnamed"
+        request = f"""
+                UPDATE total_tasks 
+                SET name = "{name1}", 
+                    date = "{date1}", 
+                    text = "{text1}", 
+                    date_until = "{date_until1}"
+                WHERE id = {task_id}
+                """
+        print(request)
+        self.cur.execute(request)
         self.commit()
+    def done(self, task_id):
+
+        request = f"""
+        UPDATE total_tasks
+        SET status = "Done"
+        WHERE id = "{task_id}"
+        """
+        self.cur.execute(request)
+        self.commit()
+    def grid(self, task_id, grid):
+        request = f"""
+        UPDATE total_tasks
+        SET grid = "{grid}"
+        WHERE id = "{task_id}"
+        """
+        self.cur.execute(request)
+        self.commit()
+
     def commit(self):
         self.con.commit()
 __all__ = ["run", "sqlite3"]
